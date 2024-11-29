40598b296de6f11108bc1969ca89876133ed970f
Sohan Ahmed <157308345+sohanahmed123@users.noreply.github.com>
2024-11-29 22:59:17 +0600

    Update Accending_sorting.c

--- Accending_sorting.c
+++ Accending_sorting.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
+    int n ;
+    scanf("%d ",&n);
     int a[n];
-    for(int i=0;i<n;i++)
+    for(int  i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
